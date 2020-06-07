582465345c4a6e4e1469335e021aaccfe45bb9f3
Partho P. Das <parthopdas@live.com>
2020-06-07 17:06:08 +1000

    Initial work + AN processing

---
+++ makeCorpus.ps1
@@ -0,0 +1,18 @@
+# powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\Users\partho\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\partho\Anaconda3' ; cd d:\src\gh\bdhrs-wf"
+# & 'C:\Users\partho\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\partho\Anaconda3' ; cd d:\src\gh\bdhrs-wf
+#
+# conda create --name bodhirasa python=3.7.2
+# conda install pandas nltk matplotlib seaborn jupyter
+# conda activate bodhirasa
+
+$rootFolder = $PSScriptRoot
+$suttaName = "an"
+$corpusRoot = "D:\src\gh\sc\bilara-data\root\pli\ms\sutta\$suttaName"
+$suttaText = "$rootFolder\$suttaName.txt"
+
+Get-ChildItem -rec -filt * -file $corpusRoot | ForEach-Object {
+  $json = Get-Content $_.FullName -Encoding UTF8 | ConvertFrom-Json
+  $json | Get-Member -MemberType NoteProperty | ForEach-Object {
+    $json.($_.Name)
+  }
+}
